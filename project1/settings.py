04ae8d3c134e8392b812d40500b07f4bf5393f61
Priyanka-Codenicely <priyanka.dodeja@codenicely.in>
2020-01-24 12:06:27 +0530

    24 Jan

--- project1/settings.py
+++ project1/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
-    'materialize',
+    # 'materialize',
     'api',
     'crispy_forms',
     'responsive',
